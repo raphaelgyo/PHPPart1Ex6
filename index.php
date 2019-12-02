ef1882d3cbb22bb6cf054a127532d0b5e2b1d0bb
raphaelgyo <raphael.granizo@gmail.com>
2019-12-02 18:42:02 +0100

    1st attempt

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie 1 Exercice 6</title>
+  </head>
+  <body>
+    <p>
+    <!--ici on insère le PHP qui est lié à l'affichage-->
+      <?php
+        $number = 140;
+        echo ($number + 30) / 2;
+      ?>
+    </p>
+  </body>
+</html>
