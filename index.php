c5f9d63590a9de5d446589b2039075d31274b667
raphaelgyo <raphael.granizo@gmail.com>
2019-12-04 18:39:42 +0100

    correction

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
+<?php
+// déclaration d'une variable 'number' initialisée à '140'
+$number = 140;
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8" />
-    <title>PHP Partie 1 Exercice 6</title>
+    <title>PHP Partie1 exo6</title>
   </head>
   <body>
     <p>
-    <!--ici on insère le PHP qui est lié à l'affichage-->
       <?php
-        $number = 140;
-        echo ($number + 30) / 2;
+      /*Affichage du résultat souhaité grâce à 'echo'
+      >>>>> version sur une ligne, plus simple, plus rapide, moins fastidieux !! */
+      echo ($number + 30)/2;
+      ?>
+    </p>
+    <p>
+      <?php
+      /* version pour shadok, mais au moins on sait
+      que c'est possible de faire comme ça, ici on vient modifier la variable ligne par ligne
+      en détaillant le calcul */
+        $number = $number + 30;
+        $number = $number / 2;
+        echo $number;
+      ?>
+    </p>
+    <p>
+      <?php
+      /* autre version pour shadok: version 'simplifiée' de la précédente */
+        $number += 30;
+        $number /= 2;
+        echo $number;
       ?>
     </p>
   </body>
